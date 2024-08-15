fe287035a7eae3e356a1e622f1df403703b42dad
Gyenge László <a.gyenge.laszlo@gmail.com>
2024-08-15 16:13:03 +0200

    github test

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,10 @@ import { FormioAppConfig } from '@formio/angular';
 import { FormioAuthConfig } from '@formio/angular/auth';
 
 export const AppConfig: FormioAppConfig = {
-  appUrl: 'http://localhost:3001',
-  apiUrl: 'http://localhost:3001',
+  // appUrl: 'http://localhost:3001',
+  // apiUrl: 'http://localhost:3001',
+  appUrl: 'https://trai.dimeb.hu',
+  apiUrl: 'https://trai.dimeb.hu',
   icons: 'fontawesome'
 };
 
