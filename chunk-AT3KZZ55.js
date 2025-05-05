e316575463a78d96beb16a15551952fbec9f486f
Gyenge Laszlo <a.gyenge.laszlo@gmail.com>
2025-05-05 16:50:36 +0200

    Auto-generated commit

--- chunk-AT3KZZ55.js
+++
@@ -1,1 +0,0 @@
-import{P as o,m as r}from"./chunk-P2TSKTQU.js";import{pa as t}from"./chunk-7W2PT5UY.js";var f=(a,i)=>{let e=t(o),u=t(r);return e.authenticated?!0:(u.navigate(["/auth/login"]),!1)};export{f as a};
---
+++ chunk-NDEBFEYH.js
@@ -0,0 +1,1 @@
+import{P as o,m as e}from"./chunk-LK7GDLPJ.js";import{pa as r}from"./chunk-CHBFHPN6.js";var m=(a,n)=>{let t=r(o),u=r(e);return t.ready.then(()=>(console.log("authGuard ready",t.authenticated),t.authenticated?!0:u.parseUrl("/auth/login")))};export{m as a};
